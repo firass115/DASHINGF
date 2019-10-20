7bdbce0d44ab437768ebc7f033d0a5e125b526e3
firass115 <firass@live.fr>
2019-10-20 21:40:02 +0200

    feat: add layouts suite commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,16 +9,6 @@ import { HomeComponent } from './pages/home/home.component';
 import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
-  /** { path: '', redirectTo: '/user/login', pathMatch: 'full' },
-  {
-    path: 'user',
-    component: UserComponent,
-    children: [
-      { path: 'registration', component: RegistrationComponent },
-      { path: 'login', component: LoginComponent }
-    ]
-  },
-  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] }**/
   { path: '', redirectTo: '/user/login', pathMatch: 'full' },
   {
     path: 'user',
